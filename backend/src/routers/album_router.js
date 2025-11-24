fa84c8f55be93c8e67ddda967f26ca367dbb4a0e
aswin402 <aswin402@gmail.com>
2025-11-24 17:37:39 +0530

    feat: backend album router and album controller

--- backend/src/routers/album_router.js
+++ backend/src/routers/album_router.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import { getAlbums, getAlbumsById } from "../controller/album_controller.js";
 
 const router = Router();
 
-router.get('/albums', (req, res) => {
-  res.send('Album Router is working!');
-});
+router.get('/', getAlbums);
+router.get('/:albumId', getAlbumsById);
 
-export default router;
+export default router;router.get('/', getAlbums);
