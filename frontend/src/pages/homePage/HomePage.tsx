bc0f6ac7559956dff2cffc02ec96b0a05ea4f415
aswin402 <aswin402@gmail.com>
2025-12-02 21:29:41 +0530

    feat: added madeForYouSongs and trendingSongs components and sectionGrid,sectionGridSkeleton

--- frontend/src/pages/homePage/HomePage.tsx
+++ frontend/src/pages/homePage/HomePage.tsx
@@ -2,6 +2,8 @@ import FeaturedSection from '@/components/FeaturedSection';
 import Topbar from '../../components/Topbar';
 import { useMusicStore } from '@/stores/useMusicStore';
 import { useEffect } from 'react';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import SectionGrid from '@/components/SectionGrid';
 
 const HomePage = () => { 
     const {
@@ -22,9 +24,18 @@ useEffect(() => {
 
 
     return (
-        <div className="rounded-md overflow-hidden">
+        <div className="rounded-md overflow-hidden h-full bg-linear-to-b from-gray-900 to-black-600">
             <Topbar />
-            <FeaturedSection />
+           <ScrollArea className="h-[calc(100vh-180px)]">
+            <div className="p-4 sm:p-6">
+                <h1 className="text-2xl sm:text-3xl font-bold mb-6">Good Morning</h1>
+                 <FeaturedSection />
+            </div>
+            <div className="space-y-8">
+                <SectionGrid title="Made for you" songs={madeForYouSongs} isLoading={isLoading}/>
+                <SectionGrid title="Trending" songs={trendingSongs} isLoading={isLoading}/>
+            </div>
+           </ScrollArea>
         </div>
     );
 };
